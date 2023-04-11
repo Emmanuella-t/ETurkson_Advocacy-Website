ce9aeffa7f832f72c5ba51df19f5df2922898ff5
Emmanuella-t <20784903-Emmanuella-t@users.noreply.replit.com>
2023-04-11 01:42:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+themeButton = document.getElementById("theme-button");
+
+// TODO: Complete the toggleDarkMode function
+const toggleDarkMode = () => {
+
+    document.body.classList.toggle("dark-mode");
+
+}
+
+
+themeButton.addEventListener("click", toggleDarkMode);
